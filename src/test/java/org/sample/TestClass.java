70eb1d2f1255e7b06fbbd3138301de0fa651f365
Ganapathi1997 <thambukarthick7@gmail.com>
2021-04-14 23:17:56 +0530

    Updated from Raja

--- src/test/java/org/sample/TestClass.java
+++ src/test/java/org/sample/TestClass.java
@@ -5,6 +5,7 @@ public class TestClass {
 	public static void getValue() {
 		System.out.println("TEST1=======");
 		System.out.println("GANAPATHI");
+		System.out.println("RAJA");
 	}
 	public static void main(String[] args) {
 		
